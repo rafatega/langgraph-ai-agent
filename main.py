929316288f1ff3988c16067e7e94e66ad09f93fb
RafaTega <rafael_tegazzini@hotmail.com>
2025-09-25 22:59:32 -0300

    Subindo primeiro código com configuração inicial

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from langgraph-ai-agent!")
+from typing import Dict, TypedDict
+from langgraph.graph import StateGraph
+from IPython.display import Image, display
+
+# Agora, vou criar o AgentState
 
 
-if __name__ == "__main__":
-    main()
+class AgentState(TypedDict):  # Schema do estado
+    message: str
+
+
+def greeting_node(state: AgentState) -> AgentState:
+    """Um nó simples que adiciona uma saudação a mensagem."""
+    state['message'] = "Olá " + state['message'] + ", como está sendo seu dia?"
+
+    return state
+
+
+graph = StateGraph(AgentState)
+
+graph.add_node("Saudacao", greeting_node)
+
+graph.set_entry_point("Saudacao")
+graph.set_finish_point("Saudacao")
+
+app = graph.compile()
+
+display(Image(app.get_graph().draw_mermaid_png()))
